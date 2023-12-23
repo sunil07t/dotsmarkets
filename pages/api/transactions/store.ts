3ca10ab618a407862199dbc978149ed2746d3417
Sunil Thapa <sunil07t@gmail.com>
2023-12-23 18:35:06 +0545

    Update store.ts

--- pages/api/transactions/store.ts
+++ pages/api/transactions/store.ts
@@ -51,36 +51,43 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
       body: JSON.stringify({ hash }),
     });
-    const subscanData : any = await subscanResponse.json();
 
-    const isValidTransaction = (subscanData: SubscanData) =>
-    subscanData.data &&
-    subscanData.data.event.some((event: Event) =>
-        event.module_id === "balances" &&
-        event.event_id === "Transfer" 
-        && event.params.includes("0xb0ec462fdf84e74f0897b813c61ba48588d55aafcab4f6467ad13648e9ca16d8")
-    ) &&
-    subscanData.data.params.some((param: Param) =>
-        param.name === "calls" &&
-        param.value.some((call: Call) =>
-        call.call_module === "System" &&
-        call.call_name === "remark_with_event" &&
-        call.params.some((p: ParamInternal) => p.name === "remark" && p.value === "{\"p\":\"poz-20\",\"op\":\"mint\",\"tick\":\"poza\",\"amt\":\"1000\"}")
-        )
-    );
+    if (subscanResponse.ok && subscanResponse.headers.get('content-type')?.includes('application/json')) {
+        const subscanData : any = await subscanResponse.json();
 
-    if (!isValidTransaction(subscanData)) {
-      return res.status(400).json({ message: "Invalid transaction." });
-    }
-
-    // Save transaction if valid
-    try {
-      const newTransaction = new Transaction({ hash, walletAddress });
-      const savedTransaction = await new Transaction(newTransaction).save();
-      res.status(201).json(savedTransaction);
-    } catch (error: any) {
-      res.status(500).json({ message: error.message });
-    }
+        const isValidTransaction = (subscanData: SubscanData) =>
+        subscanData.data &&
+        subscanData.data.event.some((event: Event) =>
+            event.module_id === "balances" &&
+            event.event_id === "Transfer" 
+            && event.params.includes("0xb0ec462fdf84e74f0897b813c61ba48588d55aafcab4f6467ad13648e9ca16d8")
+        ) &&
+        subscanData.data.params.some((param: Param) =>
+            param.name === "calls" &&
+            param.value.some((call: Call) =>
+            call.call_module === "System" &&
+            call.call_name === "remark_with_event" &&
+            call.params.some((p: ParamInternal) => p.name === "remark" && p.value === "{\"p\":\"poz-20\",\"op\":\"mint\",\"tick\":\"poza\",\"amt\":\"1000\"}")
+            )
+        );
+    
+        if (!isValidTransaction(subscanData)) {
+          return res.status(400).json({ message: "Invalid transaction." });
+        }
+    
+        // Save transaction if valid
+        try {
+          const newTransaction = new Transaction({ hash, walletAddress });
+          const savedTransaction = await new Transaction(newTransaction).save();
+          res.status(201).json(savedTransaction);
+        } catch (error: any) {
+          res.status(500).json({ message: error.message });
+        }
+      } else {
+        const text = await subscanResponse.text();
+        console.error('Unexpected response:', text);
+        return res.status(500).json({ message: 'Invalid server response' });
+      }
   } else {
     res.status(405).end();
   }
